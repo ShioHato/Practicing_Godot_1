4a4f17a53fbc0ebcd1f9b464112584ab883b9abd
Shio-chan 3 <shiohato3@gmail.com>
2024-09-15 20:48:54 +0200

    Chapter 5 done
    Chapter 5 done the kill zone

---
+++ scripts/killzone.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(body: Node2D) -> void:
+	print("You die!")
+	timer.start()
+	
+
+
+func _on_timer_timeout() -> void:
+	get_tree().reload_current_scene()
