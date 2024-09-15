19b9a5f10144bf3b61105bfe0e6e7094f5ba6b6b
Shio-chan 3 <shiohato3@gmail.com>
2024-09-15 22:32:19 +0200

    Chapter 12 done
    Chapter 12 done Music and animation

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _on_body_entered(_body: Node2D) -> void:
 	game_manager.add_point()
 	#print("+1 coin! crack!")
-	queue_free()
+	#queue_free()
+	animation_player.play("pickup")
