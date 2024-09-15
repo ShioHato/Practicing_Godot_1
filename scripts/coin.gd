80add041019781250a5bcce648226018b348919e
Shio-chan 3 <shiohato3@gmail.com>
2024-09-15 22:11:23 +0200

    Chapter 11 done
    Chapter 11 done GAME MANAGER

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("+1 coin! crack!")
+	game_manager.add_point()
+	#print("+1 coin! crack!")
 	queue_free()
